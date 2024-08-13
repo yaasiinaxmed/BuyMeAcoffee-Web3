856f6e4dfa577eead67d08f8f0929c2f452d8fd6
Yaska Boy <109895325+yaasiinaxmed@users.noreply.github.com>
2024-08-13 21:02:52 +0000

    coffee component

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,11 +3,11 @@ import React from "react";
 function Nav() {
   return (
     <div className="fixed top-[10px] w-full flex items-center justify-between container mx-auto p-6">
-      <h2 className="text-white font-bold">
+      <h2 className="text-xl text-white font-bold">
         Buy Me a <span className="text-[#DB6804]">Coffee</span>{" "}
       </h2>
       {/* connect Wallet */}
-      <button className="p-3 px-5 bg-[#DB6804] rounded-lg text-white">
+      <button className="p-3 px-5 bg-[#DB6804] rounded-lg text-white hover:scale-105">
         Connect Wallet
       </button>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
+import Coffee from "./components/Coffee";
 
 export default function Home() {
   return (
-    <main className="flex h-[100vh] container mx-auto flex-col items-center justify-center p-6 md:p-12">
+    <main className="flex h-[100vh] container mx-auto flex-col gap-3 items-center justify-center p-6 md:p-12">
       {/* Navigation */}
       <Nav />
       {/* Content */}
       <Profile />
+      <Coffee />
     </main>
   );
 }
